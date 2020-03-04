c4517b3121f20df95e4a4c896b96c801f136741d
Zsolt Molnar <ysolt@cloudera.com>
2020-03-04 08:58:48 +0100

    Move iam, s3 and vpc under modules directory

--- examples/full/main.tf
+++ examples/full/main.tf
@@ -1,5 +1,5 @@
 module "cdp-vpc" {
-  source = "../../vpc"
+  source = "../../modules/vpc"
 
   PREFIX = var.deployment_name_prefix
   VPC_CIDR = var.vpc_cidr
@@ -7,13 +7,13 @@ module "cdp-vpc" {
 }
 
 module "cdp-s3" {
-  source = "../../s3"
+  source = "../../modules/s3"
 
   DATALAKE_BUCKET = var.bucket_name
 }
 
 module "cdp-iam" {
-  source = "../../iam"
+  source = "../../modules/iam"
 
   DATALAKE_BUCKET = var.bucket_name
   PREFIX = var.deployment_name_prefix
