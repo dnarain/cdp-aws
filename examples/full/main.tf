82c2b68adda3d24c68c591bca9b9e2b2f3b80154
Zsolt Molnar <ysolt@cloudera.com>
2020-03-04 09:50:36 +0100

    Adding extra printouts

--- examples/full/main.tf
+++ examples/full/main.tf
@@ -28,3 +28,21 @@ module "cdp-iam" {
 output "cdp-cross-account-role" {
   value = module.cross-account-role.arn
 }
+
+output "Logs-Select-an-Instance-Profile" {
+  value = "TBD"
+}
+output "Logs-Location-Base" {
+  value = "s3a://${var.bucket_name}/logs"
+}
+
+output "Ranger-Audit-Role" {
+  value = "TBD"
+}
+
+output "Data-Access-Select-an-Instance-Profile" {
+  value = "TBD"
+}
+output "Storage-Location-Base" {
+  value = "s3a://${var.bucket_name}"
+}
